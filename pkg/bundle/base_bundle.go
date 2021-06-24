1b00bf4f327793b886fa1ea3c8345dd1691a5bf6
Nir Rozenbaum <nirro@il.ibm.com>
2021-06-24 09:30:46 +0300

    formatting

--- pkg/bundle/base_bundle.go
+++ pkg/bundle/base_bundle.go
@@ -6,7 +6,7 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-const hohSystemNamespace      = "hoh-system"
+const hohSystemNamespace = "hoh-system"
 
 func NewBaseBundle() Bundle {
 	return &baseBundle{
