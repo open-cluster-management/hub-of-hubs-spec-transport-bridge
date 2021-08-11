252feb38d018bd4e391e9db00283d017d75d1534
maroon <maroonay@gmail.com>
2021-08-11 21:13:03 +0300

    modified syncservice to use the new transport message

--- pkg/transport/sync-service/sync_service.go
+++ pkg/transport/sync-service/sync_service.go
@@ -9,6 +9,7 @@ import (
 	"sync"
 
 	"github.com/go-logr/logr"
+	"github.com/open-cluster-management/hub-of-hubs-spec-transport-bridge/pkg/transport"
 	"github.com/open-horizon/edge-sync-service-client/client"
 )
 
@@ -19,16 +20,6 @@ const (
 )
 
 var errEnvVarNotFound = errors.New("not found environment variable")
-
-// SyncService abstracts Open Horizon Sync Service usage.
-type SyncService struct {
-	client    *client.SyncServiceClient
-	msgChan   chan *syncServiceMessage
-	stopChan  chan struct{}
-	startOnce sync.Once
-	stopOnce  sync.Once
-	log       logr.Logger
-}
 
 // NewSyncService returns a new instance of SyncService object.
 func NewSyncService(log logr.Logger) (*SyncService, error) {
@@ -44,10 +35,20 @@ func NewSyncService(log logr.Logger) (*SyncService, error) {
 
 	return &SyncService{
 		client:   syncServiceClient,
-		msgChan:  make(chan *syncServiceMessage),
+		msgChan:  make(chan *transport.Message),
 		stopChan: make(chan struct{}, 1),
 		log:      log,
 	}, nil
+}
+
+// SyncService abstracts Open Horizon Sync Service usage.
+type SyncService struct {
+	client    *client.SyncServiceClient
+	msgChan   chan *transport.Message
+	stopChan  chan struct{}
+	startOnce sync.Once
+	stopOnce  sync.Once
+	log       logr.Logger
 }
 
 func readEnvVars() (string, string, uint16, error) {
@@ -90,11 +91,11 @@ func (s *SyncService) Stop() {
 
 // SendAsync sends a message to the sync service asynchronously.
 func (s *SyncService) SendAsync(id string, msgType string, version string, payload []byte) {
-	message := &syncServiceMessage{
-		id:      id,
-		msgType: msgType,
-		version: version,
-		payload: payload,
+	message := &transport.Message{
+		ID:      id,
+		MsgType: msgType,
+		Version: version,
+		Payload: payload,
 	}
 	s.msgChan <- message
 }
@@ -116,9 +117,9 @@ func (s *SyncService) distributeMessages() {
 			return
 		case msg := <-s.msgChan:
 			metaData := client.ObjectMetaData{
-				ObjectID:   msg.id,
-				ObjectType: msg.msgType,
-				Version:    msg.version,
+				ObjectID:   msg.ID,
+				ObjectType: msg.MsgType,
+				Version:    msg.Version,
 			}
 
 			err := s.client.UpdateObject(&metaData)
@@ -127,7 +128,7 @@ func (s *SyncService) distributeMessages() {
 				continue
 			}
 
-			reader := bytes.NewReader(msg.payload)
+			reader := bytes.NewReader(msg.Payload)
 			err = s.client.UpdateObjectData(&metaData, reader)
 
 			if err != nil {
@@ -135,8 +136,8 @@ func (s *SyncService) distributeMessages() {
 				continue
 			}
 
-			s.log.Info("Message sent successfully", "id", msg.id, "type", msg.msgType, "version",
-				msg.version)
+			s.log.Info("Message sent successfully", "id", msg.ID, "type", msg.MsgType, "version",
+				msg.Version)
 		}
 	}
 }
