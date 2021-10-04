91581d59949822c6e4aaa4c82dd1ca3e5e839445
Tsu Phin Hee <thee@redhat.com>
2021-10-04 13:56:31 -0400

    resources that are created in hoh-system namespace shouldn't be manipulated

--- pkg/bundle/base_bundle.go
+++ pkg/bundle/base_bundle.go
@@ -37,6 +37,10 @@ func (b *baseBundle) AddDeletedObject(object metav1.Object) {
 }
 
 func (b *baseBundle) manipulate(object metav1.Object) metav1.Object {
+	if object.GetNamespace() == datatypes.HohSystemNamespace {
+		return object
+	}
+
 	b.manipulateCustomFunc(object)
 	b.manipulateNameAndNamespace(object)
 
